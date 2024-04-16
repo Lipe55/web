be6d80f2d8646f55b9210a87400f1af1c0f4ee45
Luis Felipe <luisfelipecaleal10@gmail.com>
2024-04-15 23:59:15 -0300

    add tudo

--- scriot.js
+++ scriot.js
@@ -1,12 +1,12 @@
-function toggleMode(){
-    const html = document.documentElement
-    html.classList.toggle('light')
+function toggleMode() {
+   const html = document.documentElement;
+   const img = document.querySelector('#profile img');
 
-     const img = document.querySelector('#profile img')
+   html.classList.toggle('light');
 
-     if (html.classList.contains('light')) {
-        img.setAttribute('src', './imagensrocke/lipe.jpg')
-     }else{
-        img.setAttribute('src', './imagensrocke/lipe1.jpg')
-     }
+   if (html.classList.contains('light')) {
+       img.src = './imagensrocke/lipe.jpg';
+   } else {
+       img.src = './imagensrocke/lipe1.jpg';
+   }
 }
